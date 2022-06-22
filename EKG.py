562614873dd9f09cbb3b2fc7fd44bb664207382f
Mateusz <m.parasiewicz00@gmail.com>
2022-06-22 02:52:18 +0200

    reading and close

--- EKG.py
+++ EKG.py
@@ -1,9 +1,1 @@
-#Mateusz Parasiewicz WME19BC1S1
-import wfdb
-import pywt
-import matplotlib.pyplot as mtpl
-import wfdb.processing
-import scipy
 
-def record():
-    wfdb.rdrecord('ECGPCG0065', channels=[0])
