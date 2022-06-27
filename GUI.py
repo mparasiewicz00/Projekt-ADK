27183c3bf89523e2e653de6d4e0f2d41e48a740a
Mateusz <m.parasiewicz00@gmail.com>
2022-06-27 02:25:30 +0200

    GUI 2.0 and filters

--- GUI.py
+++ GUI.py
@@ -1,87 +1,129 @@
 #Mateusz Parasiewicz WME19BC1S1
-import sys
-from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtCore import QCoreApplication
-from PyQt6.QtGui import QPixmap
+import scipy.signal
+import pandas as pd
+import numpy as np
+from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtWidgets import QFileDialog
-import wfdb
-import EKG
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from PyQt6.QtCore import QCoreApplication
 
 
-class Ui_Preprocessing_ECG(object):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1000, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
 
-    def getFiles(self):
-        global fname
-        fname = QFileDialog.getOpenFileName()
-        print(fname)
-
-    def readEcg(self):
-        record = wfdb.rdrecord(fname, channels=[0], sampfrom=0)
-        wfdb.plot_wfdb(record=record, title='Sygnał wejściowy')
-
-
-    def setupUi(self, Preprocessing_ECG):
-        Preprocessing_ECG.setObjectName("Preprocessing_ECG")
-        Preprocessing_ECG.resize(519, 272)
-        self.pushButton = QtWidgets.QPushButton(Preprocessing_ECG)
-        self.pushButton.setGeometry(QtCore.QRect(20, 60, 111, 23))
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(30, 60, 121, 41))
         self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(Preprocessing_ECG)
-        self.pushButton_2.setGeometry(QtCore.QRect(150, 240, 111, 23))
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(30, 110, 121, 41))
         self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(Preprocessing_ECG)
-        self.pushButton_3.setGeometry(QtCore.QRect(20, 150, 111, 23))
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.graphicsView = QtWidgets.QGraphicsView(Preprocessing_ECG)
-        self.graphicsView.setGeometry(QtCore.QRect(150, 30, 351, 201))
-        self.graphicsView.setObjectName("graphicsView")
-        self.pushButton_4 = QtWidgets.QPushButton(Preprocessing_ECG)
-        self.pushButton_4.setGeometry(QtCore.QRect(20, 180, 111, 23))
-        self.pushButton_4.setObjectName("pushButton_4")
-        self.pushButton_5 = QtWidgets.QPushButton(Preprocessing_ECG)
-        self.pushButton_5.setGeometry(QtCore.QRect(20, 30, 111, 23))
+        self.radioButton_1 = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton_1.setGeometry(QtCore.QRect(30, 160, 121, 41))
+        self.radioButton_1.setObjectName("radioButton_1")
+        self.radioButton_2 = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton_2.setGeometry(QtCore.QRect(30, 210, 121, 41))
+        self.radioButton_2.setObjectName("radioButton_2")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(30, 260, 121, 41))
         self.pushButton_5.setObjectName("pushButton_5")
-        self.pushButton_6 = QtWidgets.QPushButton(Preprocessing_ECG)
-        self.pushButton_6.setGeometry(QtCore.QRect(340, 240, 75, 23))
+        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_6.setGeometry(QtCore.QRect(620, 490, 121, 41))
         self.pushButton_6.setObjectName("pushButton_6")
-        self.pushButton_7 = QtWidgets.QPushButton(Preprocessing_ECG)
-        self.pushButton_7.setGeometry(QtCore.QRect(420, 240, 75, 23))
+        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_7.setGeometry(QtCore.QRect(30, 0, 121, 41))
         self.pushButton_7.setObjectName("pushButton_7")
-        self.pushButton_8 = QtWidgets.QPushButton(Preprocessing_ECG)
-        self.pushButton_8.setGeometry(QtCore.QRect(20, 120, 111, 23))
+        self.pushButton_8 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_8.setGeometry(QtCore.QRect(150, 0, 121, 41))
         self.pushButton_8.setObjectName("pushButton_8")
-        self.pushButton_9 = QtWidgets.QPushButton(Preprocessing_ECG)
-        self.pushButton_9.setGeometry(QtCore.QRect(20, 90, 111, 23))
+        self.pushButton_9 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_9.setGeometry(QtCore.QRect(270, 0, 121, 41))
         self.pushButton_9.setObjectName("pushButton_9")
-        self.pushButton_10 = QtWidgets.QPushButton(Preprocessing_ECG)
-        self.pushButton_10.setGeometry(QtCore.QRect(20, 240, 111, 23))
+        self.pushButton_10 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_10.setGeometry(QtCore.QRect(760, 490, 121, 41))
         self.pushButton_10.setObjectName("pushButton_10")
 
-        self.retranslateUi(Preprocessing_ECG)
-        QtCore.QMetaObject.connectSlotsByName(Preprocessing_ECG)
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(160, 60, 800, 420))
+        self.graphicsView.setObjectName("graphicsView")
+        self.scene = QtWidgets.QGraphicsScene()
+        self.graphicsView.setScene(self.scene)
 
-    def retranslateUi(self, Preprocessing_ECG):
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def insertData(self):
+        filename = QFileDialog.getOpenFileName()
+        path = filename[0]
+        self.path = path
+        data = pd.read_csv(path)
+        imp = data["ECG"]
+        ekg = imp
+        figure = Figure()
+        axes = figure.gca()
+        x = list(np.arange(1, len(ekg) + 1))
+        y = np.array(ekg)
+        axes.plot(x, y, "-k", label="EKG")
+        axes.legend()
+        axes.grid(True)
+
+        canvas = FigureCanvas(figure)
+        canvas.resize(3000, 400)
+        self.scene.addWidget(canvas)
+
+    def loadFile(self, name, passType):
+        data = pd.read_csv(f'{name}')
+        imp = data["ECG"]
+        b, a = scipy.signal.butter(3, 0.1, passType)
+        ekg = scipy.signal.filtfilt(b, a, imp)
+        figure = Figure()
+        axes = figure.gca()
+        x = list(np.arange(1, len(ekg) + 1))
+        y = np.array(ekg)
+        axes.plot(x, y, "-k", label="first one")
+        axes.legend()
+        axes.grid(True)
+
+        canvas = FigureCanvas(figure)
+        canvas.resize(1500, 450)
+        self.scene.addWidget(canvas)
+
+    def plotECG(self):
+        if self.radioButton_1.isChecked():
+            self.loadFile(self.path, 'lowpass')
+        elif self.radioButton_2.isChecked():
+            self.loadFile(self.path, 'highpass')
+
+    def retranslateUi(self, MainWindow):
+        self.path = ''
         _translate = QtCore.QCoreApplication.translate
-        Preprocessing_ECG.setWindowTitle(_translate("Preprocessing_ECG", "Preprocessing ECG"))
-        self.pushButton.setText(_translate("Preprocessing_ECG", "Read ECG"))
-        self.pushButton.clicked.connect(self.getFiles)
-        self.pushButton_2.setText(_translate("Preprocessing_ECG", "Clear"))
-        self.pushButton_3.setText(_translate("Preprocessing_ECG", "Waverec"))
-        self.pushButton_4.setText(_translate("Preprocessing_ECG", "Peaks detect."))
-        self.pushButton_5.setText(_translate("Preprocessing_ECG", "Select file"))
-        self.pushButton_6.setText(_translate("Preprocessing_ECG", "Back"))
-        self.pushButton_7.setText(_translate("Preprocessing_ECG", "Close"))
-        self.pushButton_7.clicked.connect(QCoreApplication.instance().quit)
-        self.pushButton_8.setText(_translate("Preprocessing_ECG", "Wavelet dec."))
-        self.pushButton_9.setText(_translate("Preprocessing_ECG", "Bandpass filter"))
-        self.pushButton_10.setText(_translate("Preprocessing_ECG", "Display"))
-
+        MainWindow.setWindowTitle(_translate("MainWindow", "EKG Preprocessing"))
+        self.pushButton.setText(_translate("MainWindow", "Wprowadz dane"))
+        self.pushButton.clicked.connect(self.insertData)
+        self.pushButton_2.setText(_translate("MainWindow", "Wejsciowy sygnal"))
+        self.radioButton_1.setText(_translate("MainWindow", "Low pass"))
+        self.radioButton_2.setText(_translate("MainWindow", "High pass"))
+        self.pushButton_5.setText(_translate("MainWindow", "Wizualizacja"))
+        self.pushButton_5.clicked.connect(self.plotECG)
+        self.pushButton_6.setText(_translate("MainWindow", "Zapisz wynik"))
+        self.pushButton_7.setText(_translate("MainWindow", "Krew"))
+        self.pushButton_8.setText(_translate("MainWindow", "EKG"))
+        self.pushButton_9.setText(_translate("MainWindow", "ICG"))
+        self.pushButton_10.setText(_translate("MainWindow", "Koniec"))
+        self.pushButton_10.clicked.connect(QCoreApplication.instance().quit)
+        self.graphicsView.show()
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Preprocessing_ECG = QtWidgets.QDialog()
-    ui = Ui_Preprocessing_ECG()
-    ui.setupUi(Preprocessing_ECG)
-    Preprocessing_ECG.show()
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec())
